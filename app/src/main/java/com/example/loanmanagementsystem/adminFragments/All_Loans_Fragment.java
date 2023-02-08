a4eb695635c85227769b21620d7e0a3946d75150
SynthiaAtieno <ondengsynthia222@gmail.com>
2023-02-08 16:14:31 +0300

    Created a graph using mpandroid library

--- app/src/main/java/com/example/loanmanagementsystem/adminFragments/All_Loans_Fragment.java
+++ app/src/main/java/com/example/loanmanagementsystem/adminFragments/All_Loans_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.loanmanagementsystem.adminFragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
@@ -16,6 +17,11 @@ import com.example.loanmanagementsystem.R;
 import com.example.loanmanagementsystem.adapter.RecyclerViewAdapter;
 import com.example.loanmanagementsystem.models.Loan;
 import com.example.loanmanagementsystem.retrofitutil.ApiClient;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +34,7 @@ import retrofit2.Response;
 public class All_Loans_Fragment extends Fragment {
 
 
+    LineChart  lineChart;
     View view;
 
     public All_Loans_Fragment() {
@@ -47,10 +54,28 @@ public class All_Loans_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_all__loans_, container, false);
 
+        lineChart = view.findViewById(R.id.line_chart);
+
+        LineDataSet lineDataSet = new LineDataSet(datavalues(),"Data set1");
+        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+        iLineDataSets.add(lineDataSet);
+
+        LineData data = new LineData(iLineDataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
         return view;
     }
+    private  ArrayList<Entry> datavalues(){
+        ArrayList<Entry> datavals = new ArrayList<>();
+        datavals.add(new Entry(0,15));
+        datavals.add(new Entry(2,25));
+        datavals.add(new Entry(3,5));
+        datavals.add(new Entry(4,3));
+        datavals.add(new Entry(5,28));
+        datavals.add(new Entry(6,9));
 
-
+        return datavals;
+    }
 
 
 }
